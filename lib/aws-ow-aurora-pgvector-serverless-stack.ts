d2c1a14dba6d7c4855d56c1c3107e5e7c03cd2bc
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-07-03 18:22:46 +0700

    Update stack `AwsOwAuroraPgvectorServerlessStack`

--- lib/aws-ow-aurora-pgvector-serverless-stack.ts
+++ lib/aws-ow-aurora-pgvector-serverless-stack.ts
@@ -1,15 +1,212 @@
 import * as cdk from 'aws-cdk-lib';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as rds from 'aws-cdk-lib/aws-rds';
+import * as kms from 'aws-cdk-lib/aws-kms';
+import { NagSuppressions } from 'cdk-nag';
 import { Construct } from 'constructs';
+import { AuroraClusterInstanceType, AuroraPort, AwsOwAuroraPgvectorServerlessStackProps } from './AwsOwAuroraPgvectorServerlessStackProps';
+import { SecretValue } from 'aws-cdk-lib';
+import { parseVpcSubnetType } from '../utils/vpc-type-parser';
+import { SubnetSelection } from 'aws-cdk-lib/aws-ec2';
 
 export class AwsOwAuroraPgvectorServerlessStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: AwsOwAuroraPgvectorServerlessStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const vpc = ec2.Vpc.fromLookup(this, `${props.resourcePrefix}-VPC-Imported`, {
+      vpcId: props.vpcId,
+    });
+    const vpcSubnetType = parseVpcSubnetType(props.vpcSubnetType);
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsOwAuroraPgvectorServerlessQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // define subnetAttributes as an array of Record<string, string> with subnetId comes from props.vpcPrivateSubnetIds and availabilityZone comes from props.vpcPrivateSubnetAzs
+    const subnetAttributes: Record<string, string>[] = props.vpcPrivateSubnetIds.map((subnetId, index) => {
+      return {
+        subnetId: subnetId,
+        availabilityZone: props.vpcPrivateSubnetAzs[index],
+        routeTableId: props.vpcPrivateSubnetRouteTableIds[index],
+        type: vpcSubnetType,
+      };
+    });
+    console.log('subnetAttributes:', JSON.stringify(subnetAttributes));
+
+    // retrieve subnets from vpc
+    const vpcPrivateISubnets: cdk.aws_ec2.ISubnet[] = subnetAttributes.map((subnetAttribute) => {
+      return ec2.Subnet.fromSubnetAttributes(this, subnetAttribute.subnetId, {
+        subnetId: subnetAttribute.subnetId,
+        availabilityZone: subnetAttribute.availabilityZone,
+        routeTableId: subnetAttribute.routeTableId,
+      });
+    });
+    const vpcSubnetSelection: SubnetSelection = vpc.selectSubnets({
+      subnets: vpcPrivateISubnets,
+      availabilityZones: props.vpcPrivateSubnetAzs,
+    });
+
+    // Create subnet group for Aurora cluster
+    const auroraSubnetGroup = new rds.SubnetGroup(this, `${props.resourcePrefix}-aurora-pgvector-serverless-subnet-group`, {
+      vpc,
+      description: 'Subnet group for Aurora Serverless cluster',
+      vpcSubnets: vpcSubnetSelection,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    const auroraKmsKey = new kms.Key(this, `${props.resourcePrefix}-aurora-pgvector-serverless-kms-key`, {
+      enabled: true,
+      enableKeyRotation: true,
+      rotationPeriod: cdk.Duration.days(90),
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      keyUsage: kms.KeyUsage.ENCRYPT_DECRYPT,
+      keySpec: kms.KeySpec.SYMMETRIC_DEFAULT,
+    });
+
+    const auroraPort = AuroraPort.PostgreSQL;
+    const auroraSecurityGroup = new ec2.SecurityGroup(this, `${props.resourcePrefix}-aurora-pgvector-serverless-security-group`, {
+      vpc,
+      allowAllOutbound: false,
+      description: 'Security group for Aurora Serverless cluster',
+    });
+    auroraSecurityGroup.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    // create custom monitoring role instead of using AWS managed policy
+    const auroraMonitoringRole = new cdk.aws_iam.Role(this, `${props.resourcePrefix}-aurora-pgvector-serverless-monitoring-role`, {
+      assumedBy: new cdk.aws_iam.ServicePrincipal('monitoring.rds.amazonaws.com'),
+      description: 'Role for RDS Enhanced Monitoring',
+      inlinePolicies: {
+        monitoringPolicy: new cdk.aws_iam.PolicyDocument({
+          statements: [
+            new cdk.aws_iam.PolicyStatement({
+              actions: [
+                'logs:CreateLogGroup',
+                'logs:PutLogEvents',
+                'logs:DescribeLogStreams',
+                'logs:DescribeLogGroups',
+                'cloudwatch:PutMetricData'
+              ],
+              resources: [
+                `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/rds/*`,
+                `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/rds/*:log-stream:*`,
+                `arn:aws:cloudwatch:${this.region}:${this.account}:*`
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+    auroraMonitoringRole.applyRemovalPolicy(cdk.RemovalPolicy.DESTROY);
+
+    // add NagSuppressions for the AwsSolutions-IAM5 warning for monitoringRole
+    NagSuppressions.addResourceSuppressions(auroraMonitoringRole, [
+      {
+        id: 'AwsSolutions-IAM5',
+        reason: 'Custom monitoring role is used instead of AWS managed policy',
+      },
+    ]);
+
+    const removalPolicy = props.deployEnvironment === 'production' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY;
+    const auroraPostgresEngineVersion = props.clusterScalabilityType === rds.ClusterScalabilityType.LIMITLESS
+      ? rds.AuroraPostgresEngineVersion.VER_16_8_LIMITLESS
+      : rds.AuroraPostgresEngineVersion.VER_17_4;
+    const auroraDatabaseCluster = new rds.DatabaseCluster(this, `${props.resourcePrefix}-aurora-pgvector-serverless-database-cluster`, {
+      engine: rds.DatabaseClusterEngine.auroraPostgres({ version: auroraPostgresEngineVersion }),
+      vpc,
+      vpcSubnets: vpcSubnetSelection,
+      securityGroups: [auroraSecurityGroup],
+      autoMinorVersionUpgrade: true,
+      serverlessV2MaxCapacity: props.serverlessV2MaxCapacity,
+      serverlessV2MinCapacity: props.serverlessV2MinCapacity,
+      writer: rds.ClusterInstance.serverlessV2(AuroraClusterInstanceType.Writer),
+      readers: [
+        rds.ClusterInstance.serverlessV2(AuroraClusterInstanceType.Reader, {
+          scaleWithWriter: true,
+        }),
+      ],
+      storageEncrypted: true,
+      storageEncryptionKey: auroraKmsKey,
+      credentials: rds.Credentials.fromPassword(props.rdsUsername, SecretValue.unsafePlainText(props.rdsPassword)),
+      removalPolicy,
+      iamAuthentication: true,
+      backup: {
+        retention: cdk.Duration.days(14),
+        preferredWindow: '03:00-04:00',
+      },
+      storageType: props.storageType,
+      defaultDatabaseName: props.defaultDatabaseName,
+      monitoringInterval: cdk.Duration.seconds(props.monitoringInterval),
+      monitoringRole: auroraMonitoringRole,
+      enableClusterLevelEnhancedMonitoring: props.deployEnvironment === 'production',
+      clusterScalabilityType: props.clusterScalabilityType,
+      instanceUpdateBehaviour: rds.InstanceUpdateBehaviour.ROLLING,
+      port: auroraPort,
+      subnetGroup: auroraSubnetGroup,
+      deletionProtection: false,
+      serverlessV2AutoPauseDuration: cdk.Duration.hours(1),
+    });
+
+    // Add suppression for the deletion protection warning
+    NagSuppressions.addResourceSuppressions(auroraDatabaseCluster, [
+      {
+        id: 'AwsSolutions-RDS10',
+        reason: 'Deletion protection is intentionally disabled for development/testing purposes',
+      },
+    ]);
+
+    // Add suppression for the default endpoint port warning
+    NagSuppressions.addResourceSuppressions(auroraDatabaseCluster, [
+      {
+        id: 'AwsSolutions-RDS11',
+        reason: 'AwsSolutions-RDS11: The RDS instance or Aurora DB cluster uses the default endpoint port.',
+      },
+    ]);
+
+    // Add suppression for backtrack warning if using PostgreSQL
+    NagSuppressions.addResourceSuppressions(auroraDatabaseCluster, [
+      {
+        id: 'AwsSolutions-RDS14',
+        reason: 'Backtrack is not supported for Aurora PostgreSQL clusters',
+      },
+    ]);
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-endpoint`, {
+      value: auroraDatabaseCluster.clusterEndpoint.hostname,
+      description: 'Aurora Endpoint',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-endpoint`,
+    });
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-arn`, {
+      value: auroraDatabaseCluster.clusterArn,
+      description: 'Aurora ARN',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-arn`,
+    });
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-security-group-id`, {
+      value: auroraSecurityGroup.securityGroupId,
+      description: 'Aurora Security Group ID',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-security-group-id`,
+    });
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-kms-key-id`, {
+      value: auroraKmsKey.keyId,
+      description: 'Aurora KMS Key ID',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-kms-key-id`,
+    });
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-kms-key-arn`, {
+      value: auroraKmsKey.keyArn,
+      description: 'Aurora KMS Key ARN',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-kms-key-arn`,
+    });
+
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-port`, {
+      value: auroraPort.toString(),
+      description: 'Aurora Port',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-port`,
+    });
+
+    // export auroraDatabaseCluster arn
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-aurora-pgvector-serverless-database-cluster-arn`, {
+      value: auroraDatabaseCluster.clusterArn,
+      description: 'Aurora Database Cluster ARN',
+      exportName: `${props.resourcePrefix}-aurora-pgvector-serverless-database-cluster-arn`,
+    });
   }
 }
