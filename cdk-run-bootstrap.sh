0d881f81b9b7ef706e7a30564283aa297728349f
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-07-04 08:40:03 +0700

    Update shell scripts

--- cdk-run-bootstrap.sh
+++ cdk-run-bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-echo "Starting CDK bootstrap with verbose logging..."
+# Load environment variables if .env file is present
+if [ -f .env ]; then
+  echo "-> .env file found. Loading environment variables..."
+  export $(cat .env | sed 's/#.*//g' | xargs)
+else
+  echo "-> .env file not found. Assuming environment variables are set in the execution environment."
+fi
+
+echo "Starting CDK bootstrap..."
 cdk bootstrap --verbose
 echo "CDK bootstrap completed."
